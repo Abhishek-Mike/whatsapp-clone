358c2a04afeb087b0e54938ce43f48b3a04f3be8
Abhishek Sadhu <abhisadhu.007@gmail.com>
2021-08-29 23:32:44 +0530

    search-chat-avatar-component

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDfbDB8DJ5i_pF9MZHROecQ8BV74S3se9o",
+    authDomain: "whatsapp-clone-aec24.firebaseapp.com",
+    projectId: "whatsapp-clone-aec24",
+    storageBucket: "whatsapp-clone-aec24.appspot.com",
+    messagingSenderId: "388650841006",
+    appId: "1:388650841006:web:3386881c383d06228e4e3f",
+    measurementId: "G-YHC7GST0N8"
+  };
